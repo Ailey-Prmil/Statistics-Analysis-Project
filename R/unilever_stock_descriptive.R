bc8298750adece13024c993ceb0caf2a4440f938
Uyen324 <uyen030204@gmail.com>
2024-11-15 20:26:58 +0700

    Add Other Tool

---
+++ R/unilever_stock_descriptive.R
@@ -0,0 +1,25 @@
+df<-read.csv(file.choose(),header=TRUE)
+df
+attach(df)
+
+mean(Close)
+max(Close)
+min(Close)
+median(Close)
+mode(Close)
+
+quantile(Close,0.25)
+quantile(Close,0.75)
+
+var(Close)
+sd(Close)
+range(Close)
+
+n_total<-sum(!is.na(Close))
+sem<-sd(Close)/sqrt(n_total)
+sem
+
+require(e1071)
+skewness(Close,type = 2)
+kurtosis(Close,type = 2)
+
